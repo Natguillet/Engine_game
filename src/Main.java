7f46cf62ed02db6cfdcbb5234784beeb40b2c002
Battiston <aurelie.battiston@efrei.net>
2017-11-28 16:08:55 -0500

    add frame

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,34 @@
 import java.util.ArrayList;
+import java.util.Timer;
+
+import static java.lang.Thread.sleep;
+
 
 public class Main {
 
+
     public static void main(String[] args) {
+        int desiredRate = 60;
+        double timeNeeded = 1000000000/desiredRate;
+
+
         Test test = new Test("test");
+
+
         Entity entity = new Entity();
         entity.getComponents().add(test);
         while(true){ // Loop game
+            double startLoopTime = java.lang.System.nanoTime();
             for (Component component: entity.getComponents()) {
                 if(component instanceof ILogicComponent){
                     ((ILogicComponent) component).update();
                 }
+            }
+            //fin de update/renderer/autre
+            double endLoopTime = java.lang.System.nanoTime() - startLoopTime;
+            while (timeNeeded - endLoopTime > 0) {
+                Thread.yield();
+                endLoopTime = java.lang.System.nanoTime();
             }
         }
     }
